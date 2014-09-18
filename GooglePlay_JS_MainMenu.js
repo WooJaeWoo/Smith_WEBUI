4e1c2d5b9d62de17650c01bae2ca443272a7f486
Smith <smithoo@nhnnext.org>
2014-09-18 15:15:46 +0900

    ReviewDay2

--- GooglePlay_JS_MainMenu.js
+++ GooglePlay_JS_MainMenu.js
@@ -2,11 +2,32 @@ window.addEventListener('load', function() {
 	var menuTrigger = document.querySelector('#main_menu_trigger');
 	var mainMenu = document.querySelector('#main_menu');
 	menuTrigger.addEventListener('mouseover', function(ev) {
+		console.log("mouserover");
 		mainMenu.style.display = "block";
-		mainMenu.style.opacity = "1";
+		var _el = ev.target;
+		var eOpacity = 0;
+		var overEvent = setInterval(function () {
+			console.log(mainMenu.style.opacity);
+			mainMenu.style.opacity = eOpacity;
+			eOpacity += 0.1;
+			if (mainMenu.style.opacity > 1)
+			{
+				clearInterval(overEvent);
+			}
+		}, 100);
 	}, false)
-	menuTrigger.addEventListener('mouseout', function(ev) {
+
+	mainMenu.addEventListener('mouseout', function(ev) {
 		mainMenu.style.display = "none";
-		mainMenu.style.opacity = "0";
-	})
+		var _el = ev.target;
+		var eOpacity = 1;
+		var overEvent = setInterval(function () {
+			mainMenu.style.opacity = eOpacity;
+			eOpacity -= 0.1;
+			if (mainMenu.style.opacity < 0)
+			{
+				clearInterval(overEvent);
+			}
+		}, 100);
+	}, false)
 }, false)
